2d8744450483e15af77ac82907a80fbf27693001
Abreham Atlaw <abreham.atlaw@yahoo.com>
2023-07-29 19:30:14 +0300

    fxes

--- src/configs/data_configs.ts
+++ src/configs/data_configs.ts
@@ -10,5 +10,5 @@ export class ApiConfigs{
 
 	// public static API_URL = "http://localhost:8888/api"
 	public static API_URL = "https://post-gallery.alwaysdata.net/api"
-	public static HOST_URL = "https://localhost:5173"
+	public static HOST_URL = "https://post-gallery.com"
 }
